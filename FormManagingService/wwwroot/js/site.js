a779fe9aff951fe260b6fbe60bdc31127d891cff
jakabij <jakabistvanj@gmail.com>
2020-05-22 17:01:22 +0200

    Login and register page added

--- FormManagingService/wwwroot/js/site.js
+++ FormManagingService/wwwroot/js/site.js
@@ -1,29 +1,52 @@
-﻿function LoadLoginPage() {
-    //ToDo the Login page loading
+﻿function hideRegister(){
+    //Hide the Register page.
+    let formBlock = document.querySelector("#registerForms");
+    formBlock.setAttribute("style", "display: none");
 }
 
 
-function LoadRegisterPage(){
-    //ToDo the loading for register page
+function hideLogin(){
+    //Hide the Login page.
+    let formBlock = document.querySelector("#loginForms");
+    formBlock.setAttribute("style", "display: none");
 }
 
 
-function LoadPage() {
-    //do some naughty things here
+
+function showLoginPage() {
+    //Load in the Login "page"
+
+    hideRegister();     //But first hide the Register page if it's clicked before.
+    let formBlock = document.querySelector("#loginForms");
+    formBlock.setAttribute("style", "display: unset");
+}
+
+
+function showRegisterPage(){
+    //Load in the Register "page"
+
+    hideLogin();        //But first hide the Login page if it's clicked before.
+    let formBlock = document.querySelector("#registerForms");
+    formBlock.setAttribute("style", "display: unset");
+}
+
+
+function loadPage() {
+    console.log("Welcome on the page!"); //TO DO THIS LATER
 }
 
 
 document.addEventListener("DOMContentLoaded", () => {
     let header = document.querySelector("#headerBlock");
 
-    let headerElement = document.querySelector("firstHeader");
-    headerElement.addEventListener("click", LoadPage);
+    let headerElement = document.querySelector("#firstHeader");
+    headerElement.addEventListener("click", loadPage);
 
     headerElement = document.createElement("a");
     headerElement.textContent = "Register";
     headerElement.setAttribute("class", "header");
     headerElement.setAttribute("id", "registerHeader");
-    headerElement.addEventListener("click", LoadRegisterPage);
+    headerElement.addEventListener("click", showRegisterPage);
 
     header.appendChild(headerElement);
 
@@ -32,7 +55,7 @@ document.addEventListener("DOMContentLoaded", () => {
     headerElement.textContent = "Login";
     headerElement.setAttribute("class", "header");
     headerElement.setAttribute("id", "loginHeader");
-    headerElement.addEventListener("click", LoadLoginPage);
+    headerElement.addEventListener("click", showLoginPage);
 
     header.appendChild(headerElement);
 })
