8f8fb7f9d2f1d44663a23de9894244e6e24a7342
jakabij <jakabistvanj@gmail.com>
2020-06-03 10:50:05 +0200

    Question creating added (not saving yet to db)

--- FormManagingService/wwwroot/js/forms.js
+++ FormManagingService/wwwroot/js/forms.js
@@ -1,6 +1,50 @@
-﻿function createFormPage(){
+﻿let questionCounter = 0;
+
+
+function saveForm(form){
+    
+
+
+    questionCounter = 0;
+}
+
+
+function addQuestion(formStack, counter){   //Generate the questions
+
+    console.log(counter)
+
+    let questionText = document.createElement("a");
+    questionText.textContent = "What is going to be the question?"
+    formStack.appendChild(questionText);
+    formStack.appendChild(document.createElement("br"));
+
+    let question = document.createElement("input");
+
+    let questionId = "question" + counter;
+    question.setAttribute("id", questionId);
+    question.setAttribute("name", questionId);
+
+    formStack.appendChild(question);
+    formStack.appendChild(document.createElement("br"));
+    questionCounter += 1;
+}
+
+
+function createFormPage(){          //int this page you can create forms and save it to the database
+    questionCounter = 0;
+
     let createFormPage = document.querySelector("#questionForms");
     createFormPage.setAttribute("style","display: unset");
+
+    let questionButton = document.createElement("input");
+    questionButton.setAttribute("type","button");
+    questionButton.setAttribute("id", "addQuestionButton");
+    questionButton.setAttribute("value", "+");
+    questionButton.addEventListener("click", ()=>{
+        addQuestion(createFormPage, questionCounter)
+    });
+
+    createFormPage.appendChild(questionButton);
 }
 
 
@@ -26,4 +70,7 @@ function showFormMakingHeader(){
     createFormHeader.addEventListener("click",createFormPage);
 
     header.appendChild(createFormHeader);
+
+    // console.log(currentProfileEmail)
+    // console.log(currentProfileID)
 }
